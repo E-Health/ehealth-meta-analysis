572aa50c581e5dff60f5e5efc172f482b828d128
Bellraj Eapen <github@gulfdoctor.net>
2015-11-14 10:09:35 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Finding Articles for Meta-Analysis
+
+> Finding the right articles for a systematic review or meta-analysis is the most challenging part. Janssens et. al has published a novel algorithm to expedite the process of finding eligible studies for meta-analysis based on the degree of co-citation. I have written a small shell script to implement the algorithm for PubMed using Entrez Direct. 
+
+## How to install:
+- Get hold of a Linux box. If you don't have one, wait for my next post!
+- Install Entrez Direct from NCBI by cutting and pasting the code below.
+```sh
+cd ~
+  perl -MNet::FTP -e \
+    '$ftp = new Net::FTP("ftp.ncbi.nlm.nih.gov", Passive => 1); $ftp->login;
+     $ftp->binary; $ftp->get("/entrez/entrezdirect/edirect.zip");'
+  unzip -u -q edirect.zip
+  rm edirect.zip
+  export PATH=$PATH:$HOME/edirect
+  ./edirect/setup.sh
+
+```
+- In the terminal window
+```sh
+- sudo apt-get install xml-twig-tools 
+```
+- Download this script to a writable folder.
+- Find the PMID of your key articles.
+```sh
+./ma.sh <PMID> <PMID> <PMID>   
+```
+- Example: ./ma.sh 18032878
+- Grab a coffee and wait :)
+- The results will be written to results.txt in the same folder
+- Results include degree of co-citation along with corresponding PMID in each line.
+- Enjoy! 
+
+## Contact US
+[Send an email](http://nuchange.ca/contact) OR [Join us on PRO{DENTS}](http://prodents.com)
+
+### For more information visit [NuChange Blog](http://nuchange.ca)
+
+### Meet Us on the IRC Channel ##ehealth 
+[![Visit our IRC channel](https://kiwiirc.com/buttons/irc.freenode.net/ehealth.png)](https://kiwiirc.com/client/irc.freenode.net/?nick=nuchange|?##ehealth)
+
+### Contribute
+pull-requests welcome
+
+## Disclaimer
+> THE AUTHOR MAKES NO CLAIM WHATSOEVER, EXPRESSED OR IMPLIED, ABOUT THE AUTHENTICITY, ACCURACY, RELIABILITY, COMPLETENESS OR TIMELINESS OF THE MATERIAL, SOFTWARE, TEXT, GRAPHICS AND LINKS GIVEN. IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. THE AUTHORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN 'AS IS' BASIS, AND THE AUTHOR HAVE NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
---
+++ ma.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+##############################################################################
+# This is an experimental shell script implementing the 
+# algorithm explained in this article:
+# Janssens, A. C. J. W., & Gwinn, M. (2015). 
+# Novel citation-based search method for scientific literature: 
+# application to meta-analyses. BMC Medical Research Methodology, 15, 84. 
+# http://doi.org/10.1186/s12874-015-0077-z
+#
+# This is for Unix/Linux based systems with bash and Entrez Direct (Not tested on Mac) 
+# Follow Instructions here to install Entrez Direct
+# http://www.ncbi.nlm.nih.gov/books/NBK179288/
+# Also refer the page above for terms and conditions of use of Entrez Direct
+# Install xml-twig-tools using the command: sudo apt-get install xml-twig-tools
+#
+# By
+# Bell R Eapen
+# http://nuchange.ca
+# Date: November 14, 2015
+# IRC: ##ehealth
+###############################################################################
+echo "Finding Articles citing the given reference....."
+for var in "$@"
+do 
+    # To prevent querying pubmed too fast
+    sleep 3
+    
+    # Message
+    echo "Processing" $var
+    
+    # Finding the articles citing the key article in STEP 1
+    # https://www.biostars.org/p/106301/   
+    # Why </dev/null See this: http://stackoverflow.com/questions/13800225/shell-script-while-read-line-loop-stops-after-the-first-line
+    esearch -db pubmed -query $var </dev/null | elink -related -name pubmed_pubmed_citedin | efetch -mode xml > $var.xml
+    
+    # On ubuntu/debian, xml_grep is in the xml-twig-tools package.
+    # Find PMIDs from the xml file
+    xml_grep 'PMID' $var.xml > $var.001
+    # Remove tags
+    sed -e 's/<[^>]*>//g' $var.001 > $var.002
+    # Remove Blank Lines
+    sed '/^\s*$/d' $var.002 > $var.003
+    # Remove duplicates
+    sort -u $var.003 >> static.txt
+    # Remove Temporary Files
+    rm $var.xml
+    rm $var.001
+    rm $var.002
+    rm $var.003
+done
+    # Final Sorting
+    # final.txt contains the articles citing the given articles in the command line.
+    sort  -u static.txt > final.txt
+    #save number of articles
+    article_no=$(cat final.txt | wc -l)	
+    # Delete static file
+    rm static.txt
+
+
+echo "Finding Co-citations. This may take several hours..........."
+while read -r var; do
+    # To prevent querying pubmed too fast
+    sleep 3
+    # Message
+    article_no=$((article_no-1))
+    echo "Processing" $var $article_no " more to process.."
+    # https://www.biostars.org/p/106301/
+    esearch -db pubmed -query $var </dev/null | elink -related -name pubmed_pubmed_refs | efetch -mode xml > $var.xml
+
+# If the xml output file is not empty
+if [ -s $var.xml ]
+    then
+    # On ubuntu/debian, xml_grep is in the xml-twig-tools package.
+    xml_grep 'PMID' $var.xml > $var.001
+    # Remove tags
+    sed -e 's/<[^>]*>//g' $var.001 > $var.002
+    # Remove Blank Lines
+    sed '/^\s*$/d' $var.002 > $var.003
+    # Remove duplicates
+    sort -u $var.003 >> static.txt
+
+    # Remove Temporary Files
+    rm $var.001
+    rm $var.002
+    rm $var.003
+fi
+    rm $var.xml
+done < final.txt
+
+# If the static.txt file is not empty
+if [ -s static.txt ]
+    then
+    # Count occurences
+    # results txt contains the co-citation frequency
+    sort static.txt | uniq -c > temp.txt
+    # Final sorting of results
+    sort -nr temp.txt > results.txt
+    # Delete static file
+    rm static.txt
+    rm temp.txt
+else
+    echo "No Co-Citation found"
+fi
+    rm final.txt
+
+#Display the results to the user
+FILE="results.txt"
+echo "*** Co-citation Frequency ***"
+cat $FILE
